e8aab5f59b195df78f8ec8ae85e5705015e6cdd3
Ved Patel <talk2ved11@gmail.com>
2024-10-23 22:11:17 -0400

    Refactor main.py to remove unnecessary print statement

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.secret_key = env_to_var('FLASK_SECRET_KEY')
 client_id = env_to_var('CLIENT_ID')
 client_secret = env_to_var('CLIENT_SECRET')
 redirect_uri = os.getenv('REDIRECT_URI')
-
+print(redirect_uri)
 scope = 'user-modify-playback-state user-read-playback-state'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
